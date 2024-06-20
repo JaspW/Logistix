8670da9355d18b6e83e604308b4b6508042c73e9
JaspW <108834366+JaspW@users.noreply.github.com>
2024-06-20 15:07:34 +0300

    OrderFormValidationTests.swift

---
+++ LogistixTests/OrderFormValidationTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine
+@testable import Logistix
+
+final class OrderFormValidationTests: XCTestCase {
+  func testOrderFormValidation() {
+
+   let orderForm = OrderForm()
+   orderForm.customerName = ""
+   orderForm.pickupAddress = "ул. Пушкина, д. Колотушкина"
+   orderForm.deliveryAddress = "ул. Лермонтова, д. 3"
+   orderForm.cargoDescription = "Мебель"
+   orderForm.weight = 150.0
+   orderForm.date = "2024-06-06"
+
+   do {
+     try orderForm.submit()
+     XCTFail("Форма должна выбрасывать ошибку при некорректном заполнении")
+   } catch {
+    CTAssertEqual(OrderFormError.invalidCustomerName)
+   }
+ }
+}
