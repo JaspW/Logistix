fd992569050ac820871041ce13a4b89ad1d5e893
JaspW <108834366+JaspW@users.noreply.github.com>
2024-06-20 15:11:33 +0300

    RequestCompletionSuccess.swift

---
+++ LogistixTests/RequestCompletionSuccess.swift
@@ -0,0 +1,33 @@
+import XCTest
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine
+@testable import Logistix
+
+final class AuthorizationTests: XCTestCase {
+    private var authViewModel: AuthViewModel!
+    
+    @MainActor override func setUp() {
+        super.setUp()
+        authViewModel = AuthViewModel()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        authViewModel = nil
+    }
+
+    func testRequestCompletionSuccess() {
+
+     let requestCompletionService = RequestCompletionService()
+     let requestID = "request123"
+     requestCompletionService.addRequest(Request(id: requestID, status: .pending))
+     do {
+       try requestCompletionService.completeRequest(requestID: requestID)
+       let request = try requestCompletionService.getRequest(requestID: requestID)
+       XCTAssertEqual(request.status, .completed)
+     } catch {
+       XCTFail("Завершение заявки не должно выбрасывать ошибку при корректном ID заявки")
+     }
+   }
+ }
